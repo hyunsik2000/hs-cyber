0034bb906dec0474653a8ff1aa6bdfc78eb85c78
hyunsik2000 <ryuhsik2000@naver.com>
2025-11-11 18:31:33 +0900

    #5 docs: 디렉토리 구조 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { Routes, Route } from "react-router";
+
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
+  const ROUTES = [{ element: <Main />, path: "/" }];
+
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route element={<Layout />} path="/">
+        {ROUTES.map((route) => (
+          <Route key={route.path} {...route} />
+        ))}
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
