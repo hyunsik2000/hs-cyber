82e81e881471c52bee0985e7b63795096f7aef84
hyunsik2000 <ryuhsik2000@naver.com>
2025-11-24 21:02:31 +0900

    #10 feat: Header Link Path 매핑

---
+++ src/constants/path.js
@@ -0,0 +1,22 @@
+export const PATH = [
+  {
+    id: 0,
+    url: "/",
+    content: "Home",
+  },
+  {
+    id: 1,
+    url: "/about",
+    content: "About",
+  },
+  {
+    id: 2,
+    url: "/contact",
+    content: "Contact",
+  },
+  {
+    id: 3,
+    url: "/blog",
+    content: "Blog",
+  },
+];
