ee5eab966e68eddb2df0d4d46cdb908de206ab7f
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-02 11:58:42 +0900

    #15 feat: CategorySection 아이콘 추가

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,3 +4,11 @@ export { default as HeartIcon } from "./Header/Heart.svg?react";
 export { default as CartIcon } from "./Header/Cart.svg?react";
 export { default as ProfileIcon } from "./Header/Profile.svg?react";
 export { default as MenuIcon } from "./Header/Menu.svg?react";
+
+export { default as MobileAccessoriesIcon } from "./CategorySection/Mobile_accessories.svg?react";
+export { default as LapTopsIcon } from "./CategorySection/Laptops.svg?react";
+export { default as MensWatchesIcon } from "./CategorySection/Mens-watches.svg?react";
+export { default as GroceriesIcon } from "./CategorySection/Groceries.svg?react";
+export { default as TabletsIcon } from "./CategorySection/Tablets.svg?react";
+export { default as SmartphonesIcon } from "./CategorySection/Smartphones.svg?react";
+export { default as ArrowIcon } from "./CategorySection/Arrow.svg?react";
